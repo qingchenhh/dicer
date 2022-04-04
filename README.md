67469549ff82695e0151bb69ea4f4f3a79dd1f3d
清晨 <2394558734@qq.com>
2022-04-04 23:30:56 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# 工具介绍
+
+嘛这个工具很简单啦。就是根据名字和简称简单做拼接生成一个小字典。
+
+没啥亮点。
+
+# 工具的使用
+
+-n 或者 --name 指定名字
+
+-a 或者 --abbreviation 指定简称
+
+``` python
+python gen_dic.py -n qingchen -a qc
+```
+
+
+
+![image-20220404232709111](images/image-20220404232709111.png)
---
+++ gen_dic.py
@@ -0,0 +1,72 @@
+import os
+import time
+import argparse
+
+dic_list = []
+
+def args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-n','--name',dest="name",required=True, type=str,help="Please input name (e.g. -n \"qingchen\")")
+    parser.add_argument('-a', '--abbreviation', dest="abbreviation", type=str, default=False,
+                        help="Please input abbreviation (e.g. -a \"qc\")")
+
+    return parser.parse_args()
+
+def time_dic(name,and_str='@'):
+    now_year = time.strftime('%Y',time.localtime())
+    last_year = str(int(time.strftime('%Y',time.localtime()))-1)
+    llast_year = str(int(time.strftime('%Y', time.localtime())) - 2)
+
+    year_list = [now_year,last_year,llast_year]
+    temp_list = ['','!','$']
+
+    for i in year_list:
+        for j in temp_list:
+            dic_list.append(name + i + j)
+            dic_list.append(name + and_str + i + j)
+            dic_list.append(name.capitalize() + i + j)
+            dic_list.append(name.capitalize() + and_str + i + j)
+            dic_list.append(name.upper() + i + j)
+            dic_list.append(name.upper() + and_str + i + j)
+
+def and_dic(name,and_str='@'):
+    with open(r'dic/base_passwd.txt',mode='r',encoding='utf-8') as f:
+        for i in f:
+            i = i.replace('\n','')
+            dic_list.append(name + i)
+            dic_list.append(name + and_str + i)
+            dic_list.append(name.capitalize() + i)
+            dic_list.append(name.capitalize() + and_str + i)
+            dic_list.append(name.upper() + i)
+            dic_list.append(name.upper() + and_str + i)
+
+def wirte_dicfile():
+    with open(r'dic/generate_dic.txt',mode='w',encoding='utf-8') as f:
+        for i in dic_list:
+            f.write(i + '\n')
+        with open('dic/password.txt',mode='r',encoding='utf-8') as f1:
+            for j in f1:
+                f.write(j)
+
+def run():
+    if not os.path.isdir('dic'):
+        print("没有找到dic目录！")
+        exit(-1)
+    elif not os.path.exists('dic/base_passwd.txt'):
+        print("没有找到base_passwd.txt文件！")
+        exit(-1)
+    elif not os.path.exists('dic/password.txt'):
+        print("没有找到password.txt文件！")
+        exit(-1)
+    if args.abbreviation:
+        time_dic(args.abbreviation)
+        and_dic(args.abbreviation)
+    time_dic(args.name)
+    and_dic(args.name)
+    wirte_dicfile()
+
+    print("[+] 字典已经生成在当前目录下的dic目录下的generate_dic.txt文件中。")
+
+if __name__ == '__main__':
+    args = args()
+    run()
