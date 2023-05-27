304608f7e4e5e86900bc9f052e706eab874e7833
清晨 <37011241+qingchenhh@users.noreply.github.com>
2023-05-27 22:21:46 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -14,28 +14,68 @@
 
 可以自己改啦，感觉字典少了就改一下base_passwd.txt的字典。
 
-base_passwd.txt的使用规律，把连接如果加入的字典有在连接字符中或者末尾字符中就去掉，比如1$，这个$因为是末尾就可以去掉，写1在base_passwd.txt
+base_passwd.txt的使用规律，把连接如果加入的字典有在连接字符中或者末尾字符中就去掉，比如`1$`，这个`$`因为是末尾就可以去掉，写`1`在base_passwd.txt
 
-因为$已经是末尾字符了，生成的密码会生成1$的可能，没有加的必要
+因为$已经是末尾字符了，生成的密码会生成`1$`的可能，没有加的必要
+
+针对国内，对国外的爆破应该并不太友好，比如你拿去打国外的靶机，应该很少能爆破出来。
+
+---
+
+最后依照管理推荐一些好用的字典：
+
+>Dictionary-Of-Pentesting：https://github.com/insightglacier/Dictionary-Of-Pentesting
+>
+>Web-Fuzzing-Box：https://github.com/gh0stkey/Web-Fuzzing-Box
+>
+>SuperWordlist：https://github.com/CrackerCat/SuperWordlist
+>
+>fuzzDicts：https://github.com/TheKingOfDuck/fuzzDicts
+>
+>Fuzz_dic：https://github.com/7hang/Fuzz_dic
+
+
+
+# 小提示
+
+dic目录下的字典都可以单独使用，特别是password.txt，如果有些网站不想通过python脚本根据名称生成字典也可以直接独立使用。特别是用户字典，比如tomcat的字典或者是ssh这些服务类的用户名其实很少是名称+admin之类的，但是为了以防万一还是生成了一个，但是其实大部分情况下还是直接用最好，用户少爆破快。
+
+渗透中实在没有突破口的时候，而且没有那种很安全的验证码的时候，就是需要跑全一点花点时间也没关系，这时候生成的字典也跑不出来密码，可以尝试上面推荐的密码，然后搞个top1万，或者top2万。
 
 # 工具的使用
 
--n 或者 --name 指定名字，可以有多个，逗号隔开，必须参数！
+用法就是`-h`选项看一下就知道了。
 
-如：
+![1685196909393](images/1685196909393.png)
+
+`-n `或者` --name `指定名字，可以有多个，逗号隔开，必须参数！如：
 
 ``` python
 python gen_dic.py -n qingchen,qc
 ```
 
+![1685195853899](images/1685195853899.png)
 
-
-![image-20220404232709111](images/jietu.png)
-
-增加字符连接符+，以上程序运行qingchen的结果只能是要么全大写要么首字母大写，但是想要得到QingChen的Chen首字母大写做不到，因为不知道单词的边界是哪里，因此可以使用+号连接。如
+以上程序运行`qingchen`的结果只能是要么全大写要么首字母大写，但是想要得到`QingChen`的`Chen`首字母大写做不到，因为不知道单词的边界是哪里，这时候就可以使用字符连接符`+`如：
 
 ```
-python gen_dic.py -n qing+chen,abc,qc
+python gen_dic.py -n qing+chen,qc
 ```
 
-![image](images/jietu2.png)
+![1685195984170](images/1685195984170.png)
+
+生成字典是各种组合再组合password.txt；而有时候，当我们单独使用了password.txt时，没有爆破出来，我们再想生成密码字典，因为password.txt的内容已经跑过了，就不想要追加这部分字典再生成的字典中，我们就可以使用`-m noadd`。如：
+
+```
+python gen_dic.py -n qing+chen,qc -m noadd
+```
+
+![1685196484274](images/1685196484274.png)
+
+如果要生成其他用户名的字典就用`-ut`指定。比如，要生成tomcat的用户。
+
+```
+python gen_dic.py -n qing+chen,qc -ut tomcat
+```
+
+![1685197020428](images/1685197020428.png)
--- gen_dic.py
+++ gen_dic.py
@@ -6,7 +6,11 @@ dic_list = []
 
 def args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-n','--name',dest="name",required=True, type=str,help="Please input name (e.g. -n \"qingchen,qc,qqqccc,qingccc\")")
+    parser.add_argument('-n','--name',dest="name",required=True, type=str,help="输入单位或者系统名 (e.g. -n \"qingchen,qc,qqqccc,qingccc\")")
+    parser.add_argument('-ut', '--usertype', dest="usertype", required=False, default='web',type=str,
+                        help="输入生成的用户名类型，可输入的类型有： base（默认）、web、rdp、mysql、mssql、ftp、ssh、tomcat)")
+    parser.add_argument('-m', '--mode', dest="mode", required=False, default='add', type=str,
+                        help="生成的字典默认会把password.txt的内容+name的组合一起写入到新的密码文件，如果不想让password.txt文件也追加到新密码文件可以使用该选项。(e.g. -m noadd)")
 
     return parser.parse_args()
 
@@ -35,13 +39,13 @@ def time_dic(name_list,and_strs=['@','.','_','']):
 
 # 拼接bash字典
 def and_dic(name_list,and_strs=['@','.','_','']):
-    temp_list = ['','!','$','.','@']
+    temp_list = ['', '!', '$', '.', '@','#']
     for end_str in temp_list:
         for and_str in and_strs:
             for name in name_list:
                 with open(r'dic/base_passwd.txt', mode='r', encoding='utf-8') as f:
                     for i in f:
-                        i = i.replace('\n','')
+                        i = i.replace('\n', '')
                         # 判断连接符是否存在结尾字符中
                         if (i.find(and_str) != -1) and (and_str != '.'):
                             str1 = name + i + end_str
@@ -75,33 +79,62 @@ def get_names(names):
     return names
 
 # 把字典写入文件
-def wirte_dicfile():
-    with open(r'dic/generate_dic.txt',mode='w',encoding='utf-8') as f:
+def wirte_dicfile(mode):
+    with open(r'password.txt',mode='w',encoding='utf-8') as f:
         for i in dic_list:
             f.write(i + '\n')
-        with open('dic/password.txt',mode='r',encoding='utf-8') as f1:
+        if (mode != "noadd"):
+            with open('dic/password.txt',mode='r',encoding='utf-8') as f1:
+                for j in f1:
+                    # 去重
+                    if j.replace('\n','') not in dic_list:
+                        f.write(j)
+    print("\n[+] 生成的密码文件为：password.txt")
+
+def gen_user(names,ut):
+    filename = ut + "_user.txt"
+    if not os.path.exists("dic/" + filename):
+        print("[-] 没有找到"+filename+"文件！")
+        return False
+    user_list = []
+    names1 = names.split(',')
+    for name in names1:
+        temp = name.replace('+','')
+        user_list.append(temp)
+        user_list.append(temp.capitalize())
+        user_list.append(temp + "admin")
+        user_list.append(temp + "_admin")
+        user_list.append(temp.capitalize() + "admin")
+        user_list.append(temp + "Admin")
+        user_list.append(temp.capitalize() + "Admin")
+        user_list.append(temp + "_admin")
+        user_list.append(temp.capitalize() + "_admin")
+    with open(r'users.txt',mode='w',encoding='utf-8') as f:
+        for i in user_list:
+            f.write(i + '\n')
+        with open('dic/'+filename,mode='r',encoding='utf-8') as f1:
             for j in f1:
                 # 去重
                 if j.replace('\n','') not in dic_list:
                     f.write(j)
+    print("\n[+] 生成的用户名文件为：users.txt")
 
-def run():
+def run(args):
     if not os.path.isdir('dic'):
-        print("没有找到dic目录！")
+        print("[-] 没有找到dic目录！")
         exit(-1)
     elif not os.path.exists('dic/base_passwd.txt'):
-        print("没有找到base_passwd.txt文件！")
+        print("[-] 没有找到base_passwd.txt文件！")
         exit(-1)
     elif not os.path.exists('dic/password.txt'):
-        print("没有找到password.txt文件！")
+        print("[-] 没有找到password.txt文件！")
         exit(-1)
     names = get_names(args.name)
+    gen_user(args.name,args.usertype)
     time_dic(names)
     and_dic(names)
-    wirte_dicfile()
-
-    print("\n[+] 字典已经生成在当前目录下的dic目录下的generate_dic.txt文件中。")
+    wirte_dicfile(args.mode)
 
 if __name__ == '__main__':
     args = args()
-    run()
+    run(args)
